9a0335550b7d959051f299d689e16a31583483a3
TranTatTri <7ran7at7ri@gmail.com>
2024-09-05 14:48:44 +0700

    up

--- mycoin-wallet/src/services/blockchainService.js
+++ mycoin-wallet/src/services/blockchainService.js
@@ -29,3 +29,45 @@ export const getTransactionHistory = async (address) => {
     amount: ethers.utils.formatEther(tx.value),
   }));
 };
+
+//Người dùng cần có khả năng "stake" (khóa) một lượng coin của họ để tham gia vào việc xác nhận giao dịch.
+let stakers = {}; // Lưu thông tin về số coin mà mỗi người dùng đang stake
+
+function stakeCoins(address, amount) {
+  if (!stakers[address]) {
+    stakers[address] = 0;
+  }
+  stakers[address] += amount; // Người dùng stake số coin vào hệ thống
+}
+
+//Hàm selectValidator sẽ chọn người xác nhận giao dịch mới dựa trên lượng coin mà họ đang stake:
+function selectValidator() {
+  let totalStake = Object.values(stakers).reduce((acc, stake) => acc + stake, 0);
+  let random = Math.random() * totalStake;
+
+  for (let [address, stake] of Object.entries(stakers)) {
+    if (random < stake) {
+      return address; // Chọn người xác nhận có số lượng stake cao nhất
+    }
+    random -= stake;
+  }
+}
+
+//Mỗi khi có giao dịch mới cần xác nhận, bạn sẽ sử dụng hàm selectValidator để chọn người xác nhận giao dịch. 
+//Người xác nhận này sẽ tạo khối mới và nhận phần thưởng dựa trên lượng coin mà họ đã stake.
+function validateTransaction(transaction) {
+  const validator = selectValidator();
+  if (validator) {
+    // Validator xác nhận giao dịch
+    console.log(`${validator} xác nhận giao dịch`);
+    // Thêm logic để validator tạo khối mới
+  }
+}
+
+//Người xác nhận giao dịch sẽ nhận một phần thưởng (reward)
+function rewardValidator(validatorAddress, rewardAmount) {
+  if (!stakers[validatorAddress]) {
+    stakers[validatorAddress] = 0;
+  }
+  stakers[validatorAddress] += rewardAmount; // Thưởng cho người xác nhận
+}
